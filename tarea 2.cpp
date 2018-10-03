2d5d19870b2381f797cd47ef1f6135237c6f024a
alejandro <43358869+alejandroTESEE@users.noreply.github.com>
2018-10-02 23:55:05 -0500

    Add files via upload

---
+++ tarea 2.cpp
@@ -0,0 +1,14 @@
+#include<stdlib.h>
+#include<stdio.h>
+int main (void)
+{
+    int  a;
+    int b;
+    int c;
+    a=5;
+    b=7;
+    c=a+b;
+    printf("el resultado es:%d",c);
+    
+    return 0;
+}
